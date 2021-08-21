1459f9077371bf3959844e6882d795fee4984457
unknown <ntokungwiazidane@yahoo.com>
2021-08-21 03:58:48 +0100

    added and initiallized hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
-import 'package:notes/screens/home_screen.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+// import 'package:notes/screens/home_screen.dart';
 // import 'package:notes/screens/add_notes_screen.dart';
 // import 'package:notes/screens/home_screen.dart';
 // import 'package:notes/screens/add_task_screen.dart';
-import 'package:notes/screens/splash_screen.dart';
+// import 'package:notes/screens/splash_screen.dart';
 import 'package:notes/screens/welcome_Screen.dart';
 // import 'package:notes/widgets/tasks.dart';
 // import 'package:notes/screens/welcome_Screen.dart';
 // import './screens/welcome_Screen.dart';
 // import 'screens/home_screen.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  var box = await Hive.openBox('settings');
+  box.put("theme", "dark");
   runApp(GetMaterialApp(
     theme: ThemeData(
       // primaryColor: Color.fromRGBO(121, 111, 193, 1),
